32568afbbc19b7f826572fb2a88bac01d09a9b2e
fluffy-diamond <heerdholariya2011@gmail.com>
2026-07-22 16:56:54 +0800

    intro

---
+++ intro.py
@@ -0,0 +1,13 @@
+print ("abc")
+name="Heer"
+age=15
+print("Hi, my name is",name,"and I am",age,"years old.")
+print(f"Hi, my name is {name} and \nI am {age} years old.")
+name=input("name:")
+age=int(input("age:"))
+age2=int(input("Sister's age:"))
+diff=age-age2
+print(f"Hi, my name is {name}.\nI am {age} years old. \nMy sister's and my age difference is {diff}.")
+
+import keyword
+print (keyword.kwlist)
