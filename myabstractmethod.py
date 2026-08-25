6f6189bc60dbf7b8f1b10e16cda64aa7e8d7c8a5
fluffy-diamond <heerdholariya2011@gmail.com>
2026-08-25 15:30:28 +0800

    animals

---
+++ myabstractmethod.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+class animal(ABC):
+    def __init__(self,name,habitat):
+        self.name=name
+        self.habitat=habitat
+
+    def display(self):
+        print(self.name,self.habitat)
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+class dog(animal):
+    def __init__(self, name, habitat,breed):
+        super().__init__(name, habitat)
+        self.breed=breed
+    def sound(self):
+        print("A dog says woof.")
+
+class bird(animal):
+    def __init__(self, name, habitat,breed):
+            super().__init__(name, habitat)
+            self.breed=breed
+    def sound(self):
+        print(self.breed,"A bird says tweet.")
+
+
+class cow(animal):
+    def __init__(self, name, habitat,breed):
+            super().__init__(name, habitat)
+            self.breed=breed
+    def sound(self):
+        print("A cow says moo.")
+
+
+
+d=dog("Happy","House","Golden Retriever")
+b=bird("Peggy","sky","parrot")
+c=cow("Agatha","grasslands","brown cow")
+
+for a in [dog,bird,cow]:
+     a.sound()
+     a.display()
